23793c3866157584200a711395be05255319373f
Hari007-2000 <huthayak@purdue.edu>
2024-11-10 18:44:11 -0500

    Delete Code_project/plot.py

--- Code_project/plot.py
+++
@@ -1,7 +0,0 @@
-import torch
-import matplotlib.pyplot as plt
-
-# Load the saved .pt file
-checkpoint = torch.load("./results/ratio_seed42_l0.01_epoch200_ratio0.8_datasetCIFAR-10_modelresnet20_samplings1/resnet20.pt")
-
-print(checkpoint.keys())  # Check available keys in the checkpoint
